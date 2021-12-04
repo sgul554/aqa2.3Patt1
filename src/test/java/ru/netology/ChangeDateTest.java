5c7b9d7e485ae67dc91f7b73412a879b70b4e914
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-12-04 23:56:21 +0300

    add test

--- src/test/java/ru/netology/ChangeDateTest.java
+++ src/test/java/ru/netology/ChangeDateTest.java
@@ -1,7 +1,32 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 class ChangeDateTest {
+    @Test
+    void shouldChangeDateForDelivery() {
+        open("http://localhost:9999/");
+        String FirstDate = DataGenerator.generateDate(3);
+        String SecondDate = DataGenerator.generateDate(6);
+        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(FirstDate);
+        $("[data-test-id=name] input").setValue(DataGenerator.generateName());
+        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone());
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=notification]")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + FirstDate));
 
+
+    }
 }
