af1e35818703f6e8261bc2db2062d3e0bfdf4184
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-12-04 23:23:27 +0300

    add dataGenerator

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,4 +1,30 @@
 package ru.netology;
 
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class DataGenerator {
+    private static Faker faker = new Faker(new Locale("ru"));
+
+    //private DataGenerator() {}
+
+
+    public static String generateCity() {
+        return faker.address().city();
+    }
+
+    public static String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateName() {
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone() {
+        return faker.phoneNumber().phoneNumber();
+    }
 }
