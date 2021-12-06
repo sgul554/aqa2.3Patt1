dc039d2e44e0634061fd730db54e7ff077214a62
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-12-06 20:27:18 +0300

    change test

--- src/test/java/ru/netology/ChangeDateTest.java
+++ src/test/java/ru/netology/ChangeDateTest.java
@@ -8,14 +8,17 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.withText;
 
 
 class ChangeDateTest {
     @Test
     void shouldChangeDateForDelivery() {
         open("http://localhost:9999/");
+
         String FirstDate = DataGenerator.generateDate(3);
         String SecondDate = DataGenerator.generateDate(6);
+
         $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
         $("[data-test-id=date] input").setValue(FirstDate);
@@ -27,6 +30,18 @@ class ChangeDateTest {
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно запланирована на " + FirstDate));
 
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(SecondDate);
+        $(".button").click();
+        $(withText("У вас уже запланирована встреча на другую дату."))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(withText("Перепланировать")).click();
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + SecondDate));
+
 
     }
+
+
 }
